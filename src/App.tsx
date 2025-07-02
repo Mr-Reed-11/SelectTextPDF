72ead8cfc22d117bc31fe926d25563ed9e3e4a0e
Vítor Ornelas <vitorcesargoncalvesornelas@gmail.com>
2025-07-02 11:11:08 -0300

    feat: initialize React app with Vite and Tailwind CSS
    - Added main application component (App.tsx) with basic layout.
    - Included Vite configuration for React and Tailwind CSS.
    - Integrated SVG assets for Vite and React logos.
    - Set up TypeScript configuration files for app and node environments.
    - Created global CSS file to import Tailwind CSS styles.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-4xl font-bold mb-4'>Select Text PDF</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
